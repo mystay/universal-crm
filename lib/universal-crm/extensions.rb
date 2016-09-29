1398c7e20c9c6452a8b4b57edc952469cfb85b09
Ben Petro <bpetro@feet.shoes>
2016-09-29 05:52:56 +0000

    style fix

--- lib/universal-crm/extensions.rb
+++ lib/universal-crm/extensions.rb
@@ -5,7 +5,7 @@ class String
     lines = s.split("\n")
     new_string = ''
     lines.each do |line|
-      if line[0,2] == '> '
+      if line[0,1] == '>'
         new_string += "."
       else
         new_string += line + "\r\n"
