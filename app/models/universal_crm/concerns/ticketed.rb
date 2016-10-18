6c6beb13ac78b11a9e291158debc18e37061e443
Ben Petro <benpetro@gmail.com>
2016-10-18 00:57:07 +0000

    document fix

--- app/models/universal_crm/concerns/ticketed.rb
+++ app/models/universal_crm/concerns/ticketed.rb
@@ -6,6 +6,10 @@ module UniversalCrm
       included do
         has_many :tickets, as: :document, class_name: 'UniversalCrm::Ticket'
         
+        def crm_name #can be easily overridden in the model
+          self.name
+        end
+        
         def open_ticket!(subject, document, title, content)
           ::UniversalCrm::Ticket.create title: title, content: content, subject: subject, document: document
         end
