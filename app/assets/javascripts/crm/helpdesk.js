bd35a583c9fb0804e80451ce4b32728cdfade42d
Ben Petro <benpetro@gmail.com>
2016-10-24 23:03:17 +0000

    removed jquery from application.js to stop conflicts

--- app/assets/javascripts/crm/helpdesk.js
+++ app/assets/javascripts/crm/helpdesk.js
@@ -1,9 +1,6 @@
-//= require jquery
-//= require jquery_ujs
 //= require react
 //= require react_ujs
 //= require bootstrap-sprockets
-//= require ./jquery-ui.min
 //= require ./components/helpdesk
 //= require ./components/helpdesk_preview
 //= require ./utils
