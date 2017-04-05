a263cac848139054744340a0fe39cb0f5cdd8818
Ben Petro <benpetro@gmail.com>
2017-04-05 01:40:57 +0000

    fixing incoming

--- app/controllers/universal_crm/concerns/home.rb
+++ app/controllers/universal_crm/concerns/home.rb
@@ -46,12 +46,27 @@ module UniversalCrm
             
             #check if the BCC is for our inbound addresses:
             if !bcc.blank?
-              config = UniversalCrm::Config.find_by(inbound_email_addresses: bcc)
               #check if it was forwarded to the bcc address:
               possible_token = bcc.split('@')[0]
-              config = UniversalCrm::Config.find_by(token: /#{possible_token}/i)
-              #To = customer, From = user
-              if !config.nil?
+              if config = UniversalCrm::Config.find_by(token: /#{possible_token}/i)
+                #To = Owner, From = user, BCC'd/forwarded to CRM
+                ticket_subject = UniversalCrm::Customer.find_by(scope: config.scope, email: /^#{from}$/i)
+                ticket_subject ||= UniversalCrm::Company.find_by(scope: config.scope, email: /^#{from}$/i) #check if there's a company now
+                ticket_subject ||= UniversalCrm::Customer.create(scope: config.scope, email: (from), name: from_name)
+                creator = Universal::Configuration.class_name_user.classify.constantize.find_by(email: /^#{from}$/i)
+                if !ticket_subject.nil? and !ticket_subject.blocked?
+                  ticket_subject.update(name: to_name) if ticket_subject.name.blank?
+                  ticket = ticket_subject.tickets.create  kind: :email,
+                                                          title: params['Subject'],
+                                                          content: params['TextBody'].hideQuotedLines,
+                                                          html_body: params['HtmlBody'].hideQuotedLines,
+                                                          scope: config.scope,
+                                                          to_email: to,
+                                                          from_email: from,
+                                                          creator: creator
+                end
+              elsif config = UniversalCrm::Config.find_by(inbound_email_addresses: bcc)
+                #To = customer, From = user
                 ticket_subject = UniversalCrm::Customer.find_by(scope: config.scope, email: /^#{to}$/i)
                 ticket_subject ||= UniversalCrm::Company.find_by(scope: config.scope, email: /^#{to}$/i) #check if there's a company now
                 ticket_subject ||= UniversalCrm::Customer.create(scope: config.scope, email: (to), name: to_name, status: :draft)
