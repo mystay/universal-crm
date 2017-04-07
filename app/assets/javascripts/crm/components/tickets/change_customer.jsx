d9bd284f2796b4742f8799fc8f73c56ef3f98480
Ben Petro <benpetro@gmail.com>
2017-04-07 00:45:28 +0000

    minor js error fixes

--- app/assets/javascripts/crm/components/tickets/change_customer.jsx
+++ app/assets/javascripts/crm/components/tickets/change_customer.jsx
@@ -1,3 +1,8 @@
+/*
+  global React
+  global ReactDOM
+  global $
+*/
 var ChangeCustomer = React.createClass({
   getInitialState: function(){
     return({
@@ -8,7 +13,7 @@ var ChangeCustomer = React.createClass({
   componentDidMount: function(){
     this.setState({customerName: this.props.ticket.subject_name});
     var _this=this;
-    var searchField = ReactDOM.findDOMNode(this.refs.searchField)
+    var searchField = ReactDOM.findDOMNode(this.refs.searchField);
     $(searchField).autocomplete({
       source: `/crm/customers/autocomplete`,
       monLength: 3,
@@ -53,12 +58,12 @@ var ChangeCustomer = React.createClass({
     if (!this.state.customerId){
       return(
         <div className="alert alert-info alert-sm">If you cannot find the correct customer, close this window and add them using the button in the menu.</div>
-      )
+      );
     }
   },
   saveNewCustomer: function(){
     var _this=this;
-    var searchField = ReactDOM.findDOMNode(this.refs.searchField)
+    var searchField = ReactDOM.findDOMNode(this.refs.searchField);
     $.ajax({
       method: 'PATCH',
       url: `/crm/tickets/${this.props.ticket.id}/update_customer?customer_id=${this.state.customerId}`,
@@ -66,6 +71,6 @@ var ChangeCustomer = React.createClass({
         _this.setState({customerName: data.ticket.subject_name, customerId: null});
         searchField.value = '';
       }
-    })
+    });
   }
 });
