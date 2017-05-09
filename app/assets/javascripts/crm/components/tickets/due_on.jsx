c8198a61429045a3b63eb689b3e5af72d16068ee
Ben Petro <benpetro@gmail.com>
2017-05-09 05:43:16 +0000

    working on editing due dates for tasks - not complete

--- app/assets/javascripts/crm/components/tickets/due_on.jsx
+++ app/assets/javascripts/crm/components/tickets/due_on.jsx
@@ -3,6 +3,22 @@
   global $
 */
 var TicketDueOn = React.createClass({
+  getInitialState: function(){
+    return({
+      editing: false,
+      dueOn: null
+    });
+  },
+  componentDidMount: function(){
+    this.setState({dueOn: this.props.dueOn});
+  },
+  componentDidUpdate: function(){
+    var _this=this;
+    $('.datepicker').datepicker({dateFormat:'yy-mm-dd', 
+      onSelect: function(date){
+        _this.handleDueOnChange(date);
+      }});
+  },
   render: function(){
     if (this.props.dueOn){
       var labelClass = 'warning';
@@ -10,10 +26,37 @@ var TicketDueOn = React.createClass({
         labelClass = 'danger';
       }
       return(
-        <span className={`label label-${labelClass}`} style={{marginLeft: (this.props.margin ? '10px' : null)}}>Due: {this.props.dueOn}</span>
+        <div>
+          <span className={`label label-${labelClass}`} style={{marginLeft: (this.props.margin ? '10px' : null)}}>Due: {this.props.dueOn}{this.editButton()}</span>
+          {this.editForm()}
+        </div>
       );
     }else{
       return(null);
+    }
+  },
+  handleDueOnChange: function(date){
+    this.setState({dueOn: date});
+  },
+  editDate: function(){
+    this.setState({editing: true});
+  },
+  editButton: function(){
+    return(
+      <i className="fa fa-pencil" onClick={this.editDate} style={{marginLeft: '10px'}} />
+    );
+  },
+  editForm: function(){
+    if (this.state.editing){
+      return(
+        <div className="row">
+          <div className="col-sm-4">
+            <div className="form-group">
+              <input type="text" className="datepicker form-control" placeholder="DD-MM-YYYY" />
+            </div>
+          </div>
+        </div>
+      );
     }
   }
 });
